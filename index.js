36122ffec6684056000dd0b742384ee59545410e
morita <moritary@capa.co.jp>
2020-05-10 10:47:49 +0900

    処理追加

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const express = require('express');
 // expressApp create
 const app = express();
 
+const bodyParser = require('body-parser')
+
 // webフォルダの中身を公開する
 app.use(express.static('web'));
+
+app.use(bodyParser.json());
 
 //ルート（http://localhost）にアクセスしてきたときにhelloを返す
 //app.get('/', (req, rest) => rest.send('hello'));
@@ -13,15 +17,57 @@ app.use(express.static('web'));
 
 // JSON 返却
 app.get('/api/v1/list', (req, rest) =>{
-    const todoList =[
-        {title: '1111111111111111111111', done: true},
-        {title: '2222222222222222222222', done: true},
-        {title: '3333333333333333333333', done: true},
-        {title: '4444444444444444444444', done: true}
-    ];
 
-    rest.json(todoList);
+    setTimeout(function(){
+        const todoList =[
+            {id:1, title: '1111111111111111111111', done: true},
+            {id:2, title: '2222222222222222222222', done: true},
+            {id:3, title: '3333333333333333333333', done: false},
+            {id:4, title: '4444444444444444444444', done: true}
+        ];
+    
+        rest.json(todoList);
+    }, 1000);
+
 });
+
+
+app.get('/api/v1/detail', (req, rest) =>{
+
+    if(req.query.id == 1){
+        setTimeout(function(){
+            const todo = {id:1, title:'1111111111111111111111', done:true, date:'2020/01/01'};
+            rest.json(todo);
+        }, 4000)
+    }else if(req.query.id == 2){
+        setTimeout(function(){
+            const todo = {id:1, title:'2222222222222222222222', done:true, date:'2020/01/01'};
+            rest.json(todo);
+        }, 1000)
+    }else if(req.query.id == 4){
+        setTimeout(function(){
+            const todo = {id:1, title:'4444444444444444444444', done:true, date:'2020/01/01'};
+            rest.json(todo);
+        }, 1000)
+    }
+
+});
+
+app.post('/api/v1/done',(req, rest) =>{
+
+    const todoData = req.body;
+    //const todoTitle = todoData.title;
+    console.log(todoData)
+    const todoItem = {
+        id: todoData.id,
+        title: todoData.title,
+        done: true
+    };
+
+    rest.json(todoItem)
+
+});
+
 
 
 //ポート3000でサーバを立てる
--- web/js/app.js
+++ web/js/app.js
@@ -14,16 +14,60 @@ function hoge(){
 
 }
 
-function apiFetch(){
+function getTodoList(){
 
-    fetch('http://localhost:3000/api/v1/list')
+    fetch('http://localhost:3000/api/v1/list',{
+        method:'GET'
+    })
         .then(function(response){
             return response.json();
         })
         .then(function(myJson){
-            console.log(JSON.stringify(myJson));
+            myJson.forEach(function(value){
+                if(value.done){
+                    var tag = '<li>'+ value.title +'<li>'
+                    $('#done').append(tag);
+
+                    getTodoDetail(value.id)
+
+                }else{
+                    var tag = '<li>'+ value.title +'<li>'
+                    $('#notyet').append(tag);
+                }
+            })
+
+            
+            
         })
-        .then(function(myJson){
-            console.log(myJson);
-        });
+}
+
+function getTodoDetail(id){
+
+    var url = 'http://localhost:3000/api/v1/detail';
+    console.log(url+'?id='+id);
+    fetch(url+'?id='+id)
+        .then(function(response){
+            console.log(response);
+            return response.json();
+        })
+        .then(function(value){
+            var tag = '<li>'+ value.title +'<li>'
+            $('#detail').append(tag);
+        })
+}
+
+
+function todoDone(){
+    const data = {"id": 1 , "title": "done11111111111", "done":false}
+    fetch('http://localhost:3000/api/v1/done',{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify(data),
+    }).then(function(response){
+        console.log(response);
+    })
+    .catch(error => console.error(error)
+    );
 }
